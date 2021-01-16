1a01dc8bc08c303cfef84e94b422371f4962d8a2
Afaq <63483862+AfaqNabi@users.noreply.github.com>
2021-01-16 14:19:22 -0700

    branch

--- App.js
+++ App.js
@@ -1,33 +1,3 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
-import { styles } from './styles/Style.js';
-import { Routes } from './src/routes/Routes'
-import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from "@react-navigation/native"
+import { Routes } from './src/Routes.js';
 
-import {Login} from './src/routes/tabs/Login.js'
-
-const LoginAuth = createStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <LoginAuth.Navigator initialRouteName='Login' screenOptions={{}} headerMode='none'>
-        <LoginAuth.Screen name="Login" component={Login} />
-        <LoginAuth.Screen name='App' component={Routes} />
-      </LoginAuth.Navigator>
-    </NavigationContainer>
-  );
-}
-
-
-function testLogin({ navigation }) {
-  return (
-    <View>
-      <Text>Hello, please log in</Text>
-      <Button title='Log In' onPress={() => {
-        navigation.navigate('App');
-      }} />
-    </View>
-  )
-}
+export default Routes;
