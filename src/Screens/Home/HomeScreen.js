c08245b56dd04b3a687a8b50145207389c2f2e88
Zeyu-Li <zeyu.li23@gmail.com>
2021-01-17 00:16:49 -0600

    finished marking up home screen

--- src/Screens/Home/HomeScreen.js
+++ src/Screens/Home/HomeScreen.js
@@ -2,10 +2,10 @@ import React, { Component, useEffect, useState } from "react";
 import { Container, Header, Content,Card,CardItem,Text,Body,Item,Icon,Input,AppRegistry} from "native-base";
 import { firebase, db } from "../../../src/firebase/config.js";
 import { Image, View, StyleSheet, TextInput } from "react-native";
-// import { Card } from "@paraboly/react-native-card";
-import {styles} from './styles.js';
-// import {renderNext} from './new_card.js';
 
+import {styles} from './styles.js';
+
+// test data is array of recipe objects
 const test_data = [{
   recipeName: "Celery",
   img: 'https://cdn.pixabay.com/photo/2016/12/26/17/28/food-1932466_1280.jpg',
@@ -16,6 +16,12 @@ const test_data = [{
 }]
 
 export default function HomeScreen() {
+  // search
+  const [ready, readyChange] = useState(false);
+  const [search, searchOnChange] = useState('');
+  const [items, changeItem] = useState(test_data);
+  // number of results
+  const [resultsNum, changeResultNum] = useState(test_data.length);
   // const x = db
   //   .collection("Recipe")
   //   .where("user", "==", "afaq@yahoo.com")
@@ -32,43 +38,7 @@ export default function HomeScreen() {
   //     return data;
   //   });
   // }
-
-  // number of results
-  let current = -1
-  let result = 3
-  const renderNext = (test_data) => {
-    current++;
-    let stars = [];
-    let i;
-    for (i = 0; i< test_data[current].rating; i++) {
-      stars.push(1);
-    }
-    for (i = test_data[current].rating; i< 5; i++) {
-      stars.push(0);
-    }
-    return (
-      <Card>
-        <CardItem header button onPress={() => {
-          // TODO: nav to item uid
-          // alert("Accessing " + stars)
-          alert("Accessing " + test_data[current].recipeName)
-        }}>
-          <Image source={{uri: test_data[current].img}} style={styles.image} />
-        </CardItem>
-        <CardItem header button onPress={() => {
-          // TODO: nav to item uid
-          alert("Accessing " + test_data[current].recipeName)
-        }}>
-          <Text style={styles.title}>{test_data[current].recipeName+ ': '}</Text>
-          <Text>{test_data[current].description}</Text>
-        </CardItem>
-        <CardItem>
-          {/* <Image style={styles.star} source={require('@expo/../../img/star_full.png')} /> */}
-          {stars.map((stater) => <Image style={styles.star} source={stater ? require(`@expo/../../img/star_full.png`) : require(`@expo/../../img/star_empty.png`)}/>)}
-        </CardItem>
-      </Card>
-    )
-  }
+  
 
   // const RecCol = db
   //   .collection("Recipe")
@@ -78,18 +48,64 @@ export default function HomeScreen() {
   //     console.log(data);
   //   });
 
-  // scroll end?
-  useEffect(() => {});
+  // number of results
+  let current = -1
+  const renderNext = (data) => {
+    current++;
+    let stars = [];
+    let i;
+    for (i = 0; i< data.rating; i++) {
+      stars.push(1);
+    }
+    for (i = data.rating; i< 5; i++) {
+      stars.push(0);
+    }
+    return (
+      <Card>
+        <CardItem header button onPress={() => {
+          // TODO: nav to item uid
+          // alert("Accessing " + stars)
+          alert("Accessing " + data.recipeName)
+        }}>
+          <Image source={{uri: data.img}} style={styles.image} />
+        </CardItem>
+        <CardItem header button onPress={() => {
+          // TODO: nav to item uid
+          alert("Accessing " + data.recipeName)
+        }}>
+          <Text style={styles.title}>{data.recipeName+ ': '}</Text>
+          <Text>{data.description}</Text>
+        </CardItem>
+        <CardItem>
+          {/* <Image style={styles.star} source={require('@expo/../../img/star_full.png')} /> */}
+          {stars.map((stater) => <Image style={styles.star} source={stater ? require(`@expo/../../img/star_full.png`) : require(`@expo/../../img/star_empty.png`)}/>)}
+        </CardItem>
+      </Card>
+    )
+  }
+
+  // renders all the items in items
+  const renderAll = () => {
+    return items.map(element => {
+      return renderNext(element);
+    })
+  }
+
+  // when search term is changed
+  useEffect(()=> {
+    // TODO: search via firebase query?
+    console.log(search)
+  }, [search]);
   return (
     <Container>
     <Header searchBar>
       <Item>
         <Icon name="ios-search" />
-        <Input placeholder="Search Foods" />
+        <Input placeholder="Search Foods" onChangeText={text => searchOnChange(text)} value={search} />
       </Item>
     </Header>
     <Content padder>
-      {renderNext(test_data)}
+      {renderAll()}
     </Content>
     </Container>
   );
