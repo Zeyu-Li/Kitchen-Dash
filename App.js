fbc03138809228ccb28c9484fc9fdc44c4f79b95
Ben Wang <xiaoyu.w.99@gmail.com>
2021-01-16 18:08:33 -0700

    Revert "Merge branch 'master' into Afaq"
    This reverts commit 9a39296386c4466bd1747ef70a11c65f87662cc8, reversing
    changes made to 291100a88024572975deecfa4e8089b335877abd.

--- App.js
+++ App.js
@@ -1,34 +1,3 @@
-import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
-import { styles } from './styles/Style.js';
-import { Routes } from './src/Routes'
-import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from "@react-navigation/native"
+import { Routes } from './src/Routes.js';
 
-import Login from './src/screens/Login/Login'
-
-const LoginAuth = createStackNavigator();
-
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <LoginAuth.Navigator initialRouteName='Login'>
-      <LoginAuth.Screen name = "Login" component={Login} />
-      <LoginAuth.Screen name='App' component={Routes} />
-      </LoginAuth.Navigator>
-    </NavigationContainer>
-  );
-}
-
-
-function testLogin({ navigation }) {
-  return (
-    <View>
-      <Text>Hello, please log in</Text>
-      <Button title='Log In' onPress={() => {
-        navigation.navigate('App');
-      }} />
-    </View>
-  )
-}
+export default Routes;
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Kitchen Dash 🍽️
-
-Share your recipes with your friends and with the world! Have a great time making cool user recipes and have fun eating! 
+# Kitchen Dash
 
 Kitchen Dash for HackED 2021, a 24 hour hackathon! 
 
